fdb762c326c2be1587f064bc95052feb154ed45a
Riva Andrea <167135900+Andrea-Riva@users.noreply.github.com>
2024-11-14 10:55:19 +0100

    Aggiunti metodi di ricerca libri
    cercaLibroByTitolo che restituisce un Libro, cercaLibroByAutore e cercaLibroByData che restituiscono una collection di Libri

--- BookRecommender/src/Main.java
+++ BookRecommender/src/Main.java
@@ -2,13 +2,20 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
         User baseUser = new User();
-        Libro l1 = baseUser.cercaLibro("La casa degli spiriti");
-        System.out.println(l1.toString());
+
+        ArrayList<Libro> solution = baseUser.cercaLibroByData("1985-04-01");
+        for(Libro l : solution) {
+            System.out.println(l.toString());
+            System.out.println("\n");
+        }
     }
 }
--- BookRecommender/src/User.java
+++ BookRecommender/src/User.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.sql.Array;
 import java.sql.Date;
 import java.util.ArrayList;
 
@@ -9,14 +10,15 @@ public class User {
     // Metodi
     public User() {
     }
-    public Libro cercaLibro(String titolo) {
+
+    public Libro cercaLibroByTitolo(String titolo) {    // Cerca libro by titolo
         String filePath = "src/data/libri.dati.csv";
         String line;
 
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             while ((line = br.readLine()) != null) {
                 String[] lineClear = line.split("; ");
-                if(lineClear[0].equals(titolo)) {
+                if (lineClear[0].equals(titolo)) {
                     return new Libro(lineClear[0], lineClear[1], lineClear[2], lineClear[3], lineClear[4],
                             lineClear[5], lineClear[6]);
                 }
@@ -27,5 +29,49 @@ public class User {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public ArrayList<Libro> cercaLibroByAutore(String autore) { // Cerca libri by autore
+        ArrayList<Libro> libri = new ArrayList<>(); // to return
+        String filePath = "src/data/libri.dati.csv";
+        String line;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            while ((line = br.readLine()) != null) {
+                String[] lineClear = line.split("; ");
+                if(lineClear[1].equals(autore)) {
+                    Libro temp = new Libro(lineClear[0], lineClear[1], lineClear[2], lineClear[3], lineClear[4],
+                            lineClear[5], lineClear[6]);
+                    libri.add(temp);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return libri;
+    }
+
+    public ArrayList<Libro> cercaLibroByData(String data) {
+        ArrayList<Libro> libri = new ArrayList<>(); // to return
+        String filePath = "src/data/libri.dati.csv";
+        String line;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            while ((line = br.readLine()) != null) {
+                String[] lineClear = line.split("; ");
+                if(lineClear[6].equals(data)) {
+                    Libro temp = new Libro(lineClear[0], lineClear[1], lineClear[2], lineClear[3], lineClear[4],
+                            lineClear[5], lineClear[6]);
+                    libri.add(temp);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return libri;
     }
 }
