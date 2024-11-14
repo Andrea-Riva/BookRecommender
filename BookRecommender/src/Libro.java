63a13557746e682efbe9c78587339a3ed71d6a77
Riva Andrea <167135900+Andrea-Riva@users.noreply.github.com>
2024-11-14 10:53:18 +0100

    Aggiunti Getter e Setter per la classe Libro

--- BookRecommender/src/Libro.java
+++ BookRecommender/src/Libro.java
@@ -22,8 +22,61 @@ public class Libro {
         this.data = data;
     }
 
+    // Getter
     public String getTitolo() {
         return this.titolo;
+    }
+    public String getAutore() {
+        return this.autore;
+    }
+    public String getDescrizione() {
+        return this.descrizione;
+    }
+    public String getCategoria() {
+        return this.categoria;
+    }
+    public String getPubblicatore() {
+        return this.pubblicatore;
+    }
+    public String getPrezzo() {
+        return this.prezzo;
+    }
+    public String getData() {
+        return this.data;
+    }
+
+    // Setter
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public void setAutore(String autore) {
+        this.autore = autore;
+    }
+
+    // Setter per la descrizione
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    // Setter per la categoria
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    // Setter per il pubblicatore
+    public void setPubblicatore(String pubblicatore) {
+        this.pubblicatore = pubblicatore;
+    }
+
+    // Setter per il prezzo
+    public void setPrezzo(String prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    // Setter per la data
+    public void setData(String data) {
+        this.data = data;
     }
 
     @Override
