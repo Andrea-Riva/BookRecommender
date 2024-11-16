24de2ed40a1356afe795a6524b607bc189d22d8f
Riva Andrea <167135900+Andrea-Riva@users.noreply.github.com>
2024-11-16 15:58:09 +0100

    Aggiunti getter, setter e toString

--- BookRecommender/src/Libreria.java
+++ BookRecommender/src/Libreria.java
@@ -6,10 +6,46 @@ public class Libreria {
     private String nome;
     private ArrayList<Libro> collectionLibri;
 
-    // Metodi
+    // Costruttore
     public Libreria(LoggedUser proprietario, String nome, ArrayList<Libro> collectionLibri) {
         this.proprietario = proprietario;
         this.nome = nome;
         this.collectionLibri = collectionLibri;
+    }
+
+    // Getter e Setter per proprietario
+    public LoggedUser getProprietario() {
+        return this.proprietario;
+    }
+    public void setProprietario(LoggedUser proprietario) {
+        this.proprietario = proprietario;
+    }
+
+    // Getter e Setter per nome
+    public String getNome() {
+        return this.nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    // Getter e Setter per collectionLibri
+    public ArrayList<Libro> getCollectionLibri() {
+        return this.collectionLibri;
+    }
+    public void setCollectionLibri(ArrayList<Libro> collectionLibri) {
+        this.collectionLibri = collectionLibri;
+    }
+
+    // Metodo toString
+    @Override
+    public String toString() {
+        String libriToString = "";   // Tutti i libri toString()
+        for(Libro l : collectionLibri) {
+            libriToString += "\n" + l.toString();
+        }
+
+        return "Proprietario: " + this.proprietario.toString() + "\nNome libreria: " + this.nome +
+                "\nLibri presenti: " + libriToString;
     }
 }
