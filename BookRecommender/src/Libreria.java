f015f28592956fa8fc04484a4f034fe7959f34a9
CostaAris <ariscosta2002@gmail.com>
2024-11-18 19:02:09 +0100

    Javadoc Libreria 1.0

--- BookRecommender/src/Libreria.java
+++ BookRecommender/src/Libreria.java
@@ -1,19 +1,21 @@
 import java.util.ArrayList;
-
+/**
+* La classe permette di creare oggetti di tipo Libreria. 
+* Ogni libreria è contraddistinta dai campi proprietario (loggedUser, cioè un oggetto che rappresenta un'utente registrato)
+* un nome (String) arbitrario scelto alla creazione e una collezione di libri (ArrayList<Libro> collectionLibri).
+* Viene inoltre riscritto il metodo toString.
+*/
 public class Libreria {
-    // Campi
     private LoggedUser proprietario;
     private String nome;
     private ArrayList<Libro> collectionLibri;
 
-    // Costruttore
     public Libreria(LoggedUser proprietario, String nome, ArrayList<Libro> collectionLibri) {
         this.proprietario = proprietario;
         this.nome = nome;
         this.collectionLibri = collectionLibri;
     }
 
-    // Getter e Setter per proprietario
     public LoggedUser getProprietario() {
         return this.proprietario;
     }
@@ -21,7 +23,6 @@ public class Libreria {
         this.proprietario = proprietario;
     }
 
-    // Getter e Setter per nome
     public String getNome() {
         return this.nome;
     }
@@ -29,15 +30,19 @@ public class Libreria {
         this.nome = nome;
     }
 
-    // Getter e Setter per collectionLibri
     public ArrayList<Libro> getCollectionLibri() {
         return this.collectionLibri;
     }
     public void setCollectionLibri(ArrayList<Libro> collectionLibri) {
         this.collectionLibri = collectionLibri;
     }
-
-    // Metodo toString
+/**
+* Metodo toString riscritto, viene inizializzata una stringa vuota libriToString.
+* Viene scandita la lista collectionLibri e per ogni elemento viene concatenato alla stringa libriToString
+* il metodo toString di ogni oggetto della lista (toString di oggetti di tipo Libro).
+* Il metodo ritorna una stringa con formato: proprietario, nome libreria (che esegue il metodo), libri
+* presenti all'interno della libreria.
+*/
     @Override
     public String toString() {
         String libriToString = "";   // Tutti i libri toString()
