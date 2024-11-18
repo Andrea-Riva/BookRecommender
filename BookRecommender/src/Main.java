d7c1bae862c617bf5c14c5d3930ff7463c49d055
Riva Andrea <167135900+Andrea-Riva@users.noreply.github.com>
2024-11-18 17:03:38 +0100

    Debug registraLibreria

--- BookRecommender/src/Main.java
+++ BookRecommender/src/Main.java
@@ -3,12 +3,14 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws Exception {
+        boolean isLogged = false;   // Token per verificare se c'è un utente loggato
         User baseUser = new User(); // Utente non loggato
         Scanner s = new Scanner(System.in);
         // Debugging ricerca libri
@@ -40,6 +42,13 @@ public class Main {
         String mail = s.nextLine();
         System.out.print("Inserisci password: ");
         String password = s.nextLine();
-        System.out.println(baseUser.login(mail, password).toString());
+        LoggedUser loggedUser = baseUser.login(mail, password);
+        System.out.println(loggedUser.toString());
+
+        // Debugging registraLibreria()
+        ArrayList<String> libri = new ArrayList<>();
+        libri.add("Le notti bianche"); libri.add("Il giorno della civetta"); libri.add("La strada"); libri.add("Il piccolo principe");
+        Libreria debugLib = loggedUser.registraLibreria("debug", libri);
+        System.out.println("\n\n" + debugLib.toString());
     }
 }
