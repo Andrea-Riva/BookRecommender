9610712ec18074f67470e116071fc8be9c4e3a87
Riva Andrea <167135900+Andrea-Riva@users.noreply.github.com>
2024-11-22 16:28:45 +0100

    Aggiunto metodo register
    Fa uso di JsonNode per gestire l'inserimento di un singolo oggetto LoggedUser in un array JSON di oggetti List<LoggedUser>

--- BookRecommender/src/JsonUtils.java
+++ BookRecommender/src/JsonUtils.java
@@ -1,3 +1,4 @@
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.util.StdDateFormat;
@@ -9,7 +10,12 @@ import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 public class JsonUtils {
-    public List<Libro> deserializeLibri() throws IOException {
+    /**
+     * Permette di deserializzare tutti i dati presenti nel dataset in JSON in una lista di libri
+     * @return una collection con tutti i libri presenti nel dataset
+     * @throws IOException
+     */
+    public List<Libro> getLibri() throws IOException {
         String jsonLibri =
                 FileUtils.readFileToString(new File("src/data/libri.json"), StandardCharsets.UTF_8); // Leggi il file JSON come stringa
 
@@ -27,5 +33,61 @@ public class JsonUtils {
 
         // Deserializza il JSON nella lista di oggetti Libro
         return mapper.readValue(jsonLibri, libroTypeReference);
+    }
+
+    /**
+     * Restituisce il root node degli utenti
+     * @return JSON Root node
+     * @throws IOException
+     */
+    public JsonNode getUtentiAsJsonNode() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        File utentiJson = new File("src/data/utenti.json");
+        return mapper.readTree(utentiJson); // Struttura del JSON
+    }
+
+    /**
+     * Partendo da un oggetto LoggedUser, crea un nodo Json da esso
+     * @param user L'utente registrato
+     * @return root JsonNode
+     */
+    public JsonNode createUserNode(LoggedUser user) {  // Crea un nodo per un oggetto LoggedUser
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.createObjectNode()
+                .put("id", user.getId())
+                .put("nome", user.getNome())
+                .put("cognome", user.getCognome())
+                .put("codiceFiscale", user.getCodiceFiscale())
+                .put("mail", user.getMail())
+                .put("password", user.getPassword());
+    }
+
+    /**
+     * Scrive in formato JSON tutti i nodi che hanno come root node il parametro del metodo sul file degli utenti
+     * @param root Il root node degli utenti
+     * @throws IOException
+     */
+    public void writeUtentiNodes(JsonNode root) throws IOException {    // Scrive il nuovo nodo LoggedUser
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writerWithDefaultPrettyPrinter().writeValue(new File("src/data/utenti.json"), root);
+    }
+
+    /**
+     * Genera un Id univoco che è costruito prendendo l'Id massimo (dunque l'ultimo Id creato) e gli aggiunge 1
+     * @return Un Id univoco
+     * @throws IOException
+     */
+    public int getUniqueId() throws IOException {
+        JsonNode root = getUtentiAsJsonNode();
+        if(!root.isArray() || root.isEmpty()) { // Se il file è vuoto, gli ID partiranno da 1
+            return 1;
+        }
+        // Se il file contiene già utenti, trova l'ID massimo esistente
+        int maxId = 0;
+        for(JsonNode userNode : root) {
+            int newId = userNode.get("id").asInt(); // Id dell'utente corrente
+            if(newId > maxId) maxId = newId;    // Confronta Id
+        }
+        return maxId + 1;   // Restituisce un Id univoco
     }
 }
--- BookRecommender/src/User.java
+++ BookRecommender/src/User.java
@@ -1,3 +1,10 @@
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -7,16 +14,28 @@ import java.util.List;
 public class User {
     public User() {}
 
+    /**
+     * Permette di cercare un libro nel dataset tramite il parametro del titolo
+     * @param titolo Il titolo del libro
+     * @return Il libro trovato
+     * @throws IOException Se la deserializzazione non va a buon fine
+     */
     public Libro searchLibroByTitolo(String titolo) throws IOException {
-        List<Libro> dataset = new JsonUtils().deserializeLibri();   // Deserializza dataset
+        List<Libro> dataset = new JsonUtils().getLibri();   // Deserializza dataset
         for(Libro l : dataset) {    // Ciclo su tutta la lista
             if(l.getTitolo().equals(titolo)) return l;  // Se i titoli corrispondono
         }
         return null;    // Libro non trovato
     }
 
+    /**
+     * Permette di cercare una serie di libri scritti dallo stesso autore
+     * @param auth L'autore del libro
+     * @return Una collection di libri aventi lo stesso autore
+     * @throws IOException Se la deserializzazione non va a buon fine
+     */
     public List<Libro> searchLibroByAuth(String auth) throws IOException {
-        List<Libro> dataset = new JsonUtils().deserializeLibri();
+        List<Libro> dataset = new JsonUtils().getLibri();
         List<Libro> libriFound = new ArrayList<Libro>();    // To return
         for(Libro l : dataset) {
             if(l.getAutore().equals(auth)) libriFound.add(l);   // Se i titoli corrispondono aggiunge il Libro alla lista
@@ -24,11 +43,38 @@ public class User {
         return libriFound;
     }
 
+    /**
+     * Permette di cercare un libro scritto da un certo autore e pubblicato in una certa data
+     * @param auth L'autore del libro
+     * @param data La data di pubblicazione del libro
+     * @return Il libro scritto da un certo autore e pubblicato in una certa data
+     * @throws IOException Se la deserializzazione non va a buon fine
+     */
     public Libro searchLibroByDataAuth(String auth, String data) throws IOException {
-        List<Libro> dataset = new JsonUtils().deserializeLibri();
+        List<Libro> dataset = new JsonUtils().getLibri();
         for(Libro l : dataset) {
             if(l.getAutore().equals(auth) && l.getData().equals(data)) return l;
         }
         return null;
+    }
+
+    /**
+     * Scrive in formato JSON i dati dell'utente appena registrato
+     * @param nome Nome dell'utente
+     * @param cognome Cognome dell'utente
+     * @param codiceFiscale Codice fiscale dell'utente
+     * @param mail Mail dell'utente
+     * @param password Password scelta dall'utente
+     * @throws Exception
+     */
+    public void register(String nome, String cognome, String codiceFiscale, String mail, String password) throws Exception {
+        JsonUtils utils = new JsonUtils();  // Utils per gestire i nodi e l'Id
+        String encryptedPwd = new SecurityUtils().encrypt(password);    // La password crittata
+        int uniqueId = utils.getUniqueId();   // L'Id univoco
+        LoggedUser newUtente = new LoggedUser(uniqueId, nome, cognome, codiceFiscale, mail, encryptedPwd);  // Crea un obj LoggedUser con i campi del register
+        JsonNode rootUsers = utils.getUtentiAsJsonNode();   // Root node degli utenti
+        ArrayNode userArrayNode = (ArrayNode) rootUsers;    // Parsing del root node in un array di nodi
+        userArrayNode.add(utils.createUserNode(newUtente)); // Aggiunge il nuovo utente all'array di nodi
+        utils.writeUtentiNodes(userArrayNode);
     }
 }
