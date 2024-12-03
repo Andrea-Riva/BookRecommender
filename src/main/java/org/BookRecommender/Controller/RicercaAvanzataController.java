352b6ef9e545ea5e2e07b944e25a5a650383fa27
= <=>
2024-12-03 19:49:14 +0100

    Add ricerca avanzata by AuthDate

--- src/main/java/org/BookRecommender/Controller/RicercaAvanzataController.java
+++ src/main/java/org/BookRecommender/Controller/RicercaAvanzataController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import org.BookRecommender.Libro;
 import org.BookRecommender.Model.LibroModel;
@@ -15,5 +16,41 @@ import java.io.IOException;
 import java.util.Objects;
 
 public class RicercaAvanzataController {
+    @FXML
+    public AnchorPane anchorPane;
+    @FXML
+    public Label loggedAccountLabel;
+    @FXML
+    public TextField authField;
+    @FXML
+    public TextField dataField;
 
+    @FXML
+    private void initialize() {
+        if (!(Objects.isNull(LoggedUserModel.user))) {   // Se l'utente è loggato
+            loggedAccountLabel.setText(LoggedUserModel.user.getMail()); // Display della mail
+        }
+    }
+    @FXML
+    private void ricercaByAutoreData() throws IOException {
+        String auth = authField.getText();      // Campo autore
+        String data = dataField.getText();      // Campo data
+        Libro found = new User().searchLibroByDataAuth(auth, data); // Effettua il metodo di ricerca
+        if(Objects.isNull(found)) { // Se il libro non è stato trovato
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Errore di ricerca");
+            alert.setHeaderText("Libro non trovato");
+            alert.setContentText("Non è stato trovato nessun libro di " + auth +
+                    " pubblicato in data " + data);
+            alert.showAndWait();
+        } else {    // Se il libro è stato trovato
+            LibroModel.libro = found;   // Assegna al model il libro trovato
+            new SceneSwitch(anchorPane, "/org/BookRecommender/View/dettagliLibroPage.fxml");    // Apre la pagina coi dettagli del libro
+        }
+    }
+
+    @FXML
+    private void goToHome() throws IOException {   // On click btn Home
+        new SceneSwitch(anchorPane, "/org/BookRecommender/View/homePage.fxml");
+    }
 }
