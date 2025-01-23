cf3150f28cef913f67f2102fd16c1eecd557d9c1
CostaAris <ariscosta2002@gmail.com>
2025-01-23 18:25:31 +0100

    Update LoginController.java

--- src/main/java/org/BookRecommender/Controller/LoginController.java
+++ src/main/java/org/BookRecommender/Controller/LoginController.java
@@ -11,7 +11,9 @@ import org.BookRecommender.User;
 
 import java.io.IOException;
 import java.util.Objects;
-
+/**
+* Classe per il Controller del login
+*/
 public class LoginController {
     @FXML
     private AnchorPane loginAnchorPane;
@@ -21,9 +23,17 @@ public class LoginController {
     private javafx.scene.control.TextField mailField;
     @FXML
     private PasswordField passField;
-
+    
+    /**
+    * Metodo per l'handling del login
+    * Se il campo mailField o passField è vuoto viene  visualizzato u ìn messaggio di errore
+    * Se le informazion inserite sono errate viene visualizzato un messaggio di errore
+    * Se il login è andato a buon fine avviene il passaggio dei parametri dell'utente tra Controller
+    *
+    * @throws Exception se si verifica un errore durante il login
+    */
     @FXML
-    private void handleLogin() throws Exception {   // Click btn Accedi
+    private void handleLogin() throws Exception {   
         if (mailField.getText().isEmpty() || passField.getText().isEmpty()) {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("Errore di accesso");
@@ -34,22 +44,33 @@ public class LoginController {
         String userMail = mailField.getText();
         String userPass = passField.getText();
         LoggedUser log = new User().login(userMail, userPass);
-        if (Objects.isNull(log)) {   // Se il login non è andato a buon fine
+        if (Objects.isNull(log)) {   
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("Errore di accesso");
             alert.setHeaderText("Mail o password sono sbagliate");
             alert.showAndWait();
-        } else {    // Se il login è andato a buon fine
-            // Passaggio dei parametri dell'utente tra Controller
+        } else {    
             LoggedUserModel.user = log;
             System.out.println(LoggedUserModel.user.toString());
             switchHomepage();
         }
     }
+   /**
+   * Cambia la scena per visualizzare la pagina di registrazione. Questo metodo viene chiamato quando l'utente 
+   * clicca sul link o bottone di registrazione. La scena viene cambiata alla pagina di registrazione.
+   * 
+   * @throws IOException se si verifica un errore nel caricare la pagina di registrazione
+   */
     @FXML
     private void switchRegistrazione() throws IOException {
         new SceneSwitch(loginAnchorPane, "/org/BookRecommender/View/registrationPage.fxml");
     }
+    /**
+   * Cambia la scena per visualizzare la homepage. Questo metodo viene chiamato dopo un login avvenuto con successo. 
+   * La scena viene cambiata alla homepage dell'applicazione.
+   * 
+   * @throws IOException se si verifica un errore nel caricare la homepage
+   */
     @FXML
     private void switchHomepage() throws IOException {
         new SceneSwitch(loginAnchorPane, "/org/BookRecommender/View/homePage.fxml");
