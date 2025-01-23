b0d40c047ed0efc394e6c46ea8cf0039a9f18b14
CostaAris <ariscosta2002@gmail.com>
2025-01-23 18:35:21 +0100

    Update RegistrationController.java

--- src/main/java/org/BookRecommender/Controller/RegistrationController.java
+++ src/main/java/org/BookRecommender/Controller/RegistrationController.java
@@ -16,6 +16,9 @@ import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
 import java.io.IOException;
 
+/**
+*Classe per il Controller della registrazione
+*/
 public class RegistrationController {
     @FXML
     private AnchorPane registrazioneAnchorPane;
@@ -33,45 +36,63 @@ public class RegistrationController {
     private javafx.scene.control.Button genPassBtn;
     @FXML
     private javafx.scene.control.Button accediBtn;
-
+    
+    /**
+    * Metodo per l'handling della registrazione
+    * Tutti i campi richiesti devono essere compilati, altrimenti viene visualizzato un messaggio di errore
+    *
+    * @throws Exception in caso di errore durante il processo di registrazione
+    */
     @FXML
-    private void handleRegistrazione() throws Exception {   // Click btn registrazione
-        // Verifica se tutti i campi sono stati compilati
+    private void handleRegistrazione() throws Exception {   
         if (nomeField.getText().isEmpty() || cognomeField.getText().isEmpty() || cfField.getText().isEmpty() ||
-                mailField.getText().isEmpty() || passwordField.getText().isEmpty()) {   // Se qualcosa non è stato compilato
-            // Mostra un messaggio di errore
+                mailField.getText().isEmpty() || passwordField.getText().isEmpty()) {   
             Alert alert = new Alert(AlertType.ERROR);
             alert.setTitle("Errore di registrazione");
             alert.setHeaderText("Campi vuoti");
             alert.setContentText("Per favore, compila tutti i campi.");
             alert.showAndWait();
-        } else {    // Tutti i campi sono stati compilati correttamente
-            String mail = mailField.getText();  // Mail per il login
-            String password = passwordField.getText();  // Password per il login
+        } else {    
+            String mail = mailField.getText();  
+            String password = passwordField.getText();  
             new User().register(nomeField.getText(), cognomeField.getText(), cfField.getText(),
-                    mailField.getText(), passwordField.getText());  // Esegui la registrazione
-            System.out.println("Registrazione eseguita con successo");  // Debug console
-            LoggedUserModel.user = new User().login(mail, password);   // Login con credenziali di registrazione
+                    mailField.getText(), passwordField.getText());  
+            System.out.println("Registrazione eseguita con successo");  
+            LoggedUserModel.user = new User().login(mail, password);  e
             switchHome();
         }
     }
-
+    
+    /**
+    * Metodo per gestire la creazione di una password casuale (Button genera password)
+    * Richiama il metodo  {@link SecurityUtils#genera}
+    * La password generata viene copiata su tastiera
+    */
     @FXML
-    private void generatePasswordField() {  // Click btn genera password
+    private void generatePasswordField() {  
         String genPass = new SecurityUtils().genera();
         passwordField.setText(genPass);
         genPassBtn.setText("Password copiata");
-        // Copia password su tastiera
         StringSelection selection = new StringSelection(genPass);
         Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
         clipboard.setContents(selection, selection);
     }
-
+    
+   /**
+   * Cambia la scena per visualizzare la pagina di login.  
+   *
+   * @throws IOException se si verifica un errore nel caricare la pagina di login
+   */
     @FXML
     private void switchAccesso() throws IOException { // Click btn ho già un account
         new SceneSwitch(registrazioneAnchorPane, "/org/BookRecommender/View/loginPage.fxml");
     }
-
+    
+    /**
+   * Cambia la scena per visualizzare la homepage. 
+   * 
+   * @throws IOException se si verifica un errore nel caricare la homepage
+   */
     @FXML
     private void switchHome() throws IOException {  // Click btn continua senza account
         new SceneSwitch(registrazioneAnchorPane, "/org/BookRecommender/View/homePage.fxml");
