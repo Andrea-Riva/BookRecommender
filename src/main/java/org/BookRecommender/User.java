143d564752ebc02d0836b914c9cb640735568194
CostaAris <ariscosta2002@gmail.com>
2025-01-24 17:14:28 +0100

    Update User.java

--- src/main/java/org/BookRecommender/User.java
+++ src/main/java/org/BookRecommender/User.java
@@ -10,26 +10,26 @@ public class User {
     }
 
     /**
-     * Permette di cercare un libro nel dataset tramite il parametro del titolo
+     * Permette di cercare un libro nel dataset tramite il parametro del titolo.
      *
-     * @param titolo Il titolo del libro
-     * @return Il libro trovato
-     * @throws IOException Se la deserializzazione non va a buon fine
+     * @param titolo Il titolo del libro.
+     * @return Il libro trovato.
+     * @throws IOException Se la deserializzazione non va a buon fine.
      */
     public Libro searchLibroByTitolo(String titolo) throws Exception {
-        List<Libro> dataset = new JsonUtils().getLibri();   // Deserializza dataset
-        for (Libro l : dataset) {    // Ciclo su tutta la lista
-            if (l.getTitolo().equalsIgnoreCase(titolo)) return l;  // Se i titoli corrispondono
+        List<Libro> dataset = new JsonUtils().getLibri();   
+        for (Libro l : dataset) {    
+            if (l.getTitolo().equalsIgnoreCase(titolo)) return l;  
         }
-        return null;    // Libro non trovato
+        return null;   
     }
 
     /**
-     * Permette di cercare una serie di libri scritti dallo stesso autore
+     * Permette di cercare una serie di libri scritti dallo stesso autore.
      *
-     * @param auth L'autore del libro
-     * @return Una collection di libri aventi lo stesso autore
-     * @throws IOException Se la deserializzazione non va a buon fine
+     * @param auth L'autore del libro.
+     * @return Una collection di libri aventi lo stesso autore.
+     * @throws IOException Se la deserializzazione non va a buon fine.
      */
     public List<Libro> searchLibriByAuth(String auth) throws IOException {
         List<Libro> dataset = new JsonUtils().getLibri();
@@ -42,12 +42,12 @@ public class User {
     }
 
     /**
-     * Permette di cercare un libro scritto da un certo autore e pubblicato in una certa data
+     * Permette di cercare un libro scritto da un certo autore e pubblicato in una certa data.
      *
-     * @param auth L'autore del libro
-     * @param data La data di pubblicazione del libro
-     * @return Il libro scritto da un certo autore e pubblicato in una certa data
-     * @throws IOException Se la deserializzazione non va a buon fine
+     * @param auth L'autore del libro.
+     * @param data La data di pubblicazione del libro.
+     * @return Il libro scritto da un certo autore e pubblicato in una certa data.
+     * @throws IOException Se la deserializzazione non va a buon fine.
      */
     public Libro searchLibroByDataAuth(String auth, String data) throws IOException {
         List<Libro> dataset = new JsonUtils().getLibri();
@@ -58,14 +58,14 @@ public class User {
     }
 
     /**
-     * Scrive in formato JSON i dati dell'utente appena registrato
+     * Scrive in formato JSON i dati dell'utente appena registrato.
      *
      * @param nome          Nome dell'utente
      * @param cognome       Cognome dell'utente
      * @param codiceFiscale Codice fiscale dell'utente
      * @param mail          Mail dell'utente
      * @param password      Password scelta dall'utente
-     * @throws Exception
+     * @throws Exception.
      */
     public void register(String nome, String cognome, String codiceFiscale, String mail, String password) throws Exception {
         JsonUtils utils = new JsonUtils(); 
@@ -82,11 +82,11 @@ public class User {
     }
 
     /**
-     * Fa il login se mail e password sono corrette
+     * Fa il login se mail e password sono corrette.
      *
      * @param mail     La mail dell'utente
      * @param password La password dell'utente
-     * @return LoggedUser utente registrato
+     * @return LoggedUser utente registrato.
      */
     public LoggedUser login(String mail, String password) throws Exception {
         SecurityUtils decypher = new SecurityUtils();   
@@ -100,11 +100,11 @@ public class User {
     }
 
     /**
-     * Cerca tutte le librerie appartenenti ad un certo utente registrato
+     * Cerca tutte le librerie appartenenti ad un certo utente registrato.
      *
-     * @param mail la mail dell'utente del quale si vogliono vedere le librerie
-     * @return una collection di tutte le librerie dell'utente cercato
-     * @throws IOException
+     * @param mail la mail dell'utente del quale si vogliono vedere le librerie.
+     * @return una collection di tutte le librerie dell'utente cercato.
+     * @throws IOException.
      */
     public List<Libreria> visualizzaLibrerieByUser(String mail) throws IOException {  
         List<Libreria> librerieFound = new ArrayList<>();
@@ -117,11 +117,11 @@ public class User {
     }
 
     /**
-     * Cerca le recensioni da tutti gli utenti per un certo libro
+     * Cerca le recensioni da tutti gli utenti per un certo libro.
      *
-     * @param titolo il titolo del libro che si vuole cercare
-     * @return una collection delle recensioni di tutti gli utenti relative al libro cercato
-     * @throws IOException
+     * @param titolo il titolo del libro che si vuole cercare.
+     * @return una collection delle recensioni di tutti gli utenti relative al libro cercato.
+     * @throws IOException.
      */
     public List<Recensione> visualizzaRecensioneByLibro(String titolo) throws IOException {
         List<Recensione> userReviews = new JsonUtils().getRecensioni();
@@ -135,10 +135,10 @@ public class User {
     }
 
     /**
-     * Visualizza le recensioni del proprietario di tutti i libri di una certa libreria
+     * Visualizza le recensioni del proprietario di tutti i libri di una certa libreria.
      *
-     * @param nome il nome della libreria
-     * @return una collection delle recensioni di tutti i libri presenti pubblicate dal proprietario della libreria
+     * @param nome il nome della libreria.
+     * @return una collection delle recensioni di tutti i libri presenti pubblicate dal proprietario della libreria.
      */
     public List<Recensione> visualizzaRecensioniByLibreria(String nome) throws IOException {
         List<Libreria> userLibs = new JsonUtils().getLibrerie();   
@@ -161,11 +161,11 @@ public class User {
     }
 
     /**
-     * Cerca i consigli da tutti gli utenti per un certo libro
+     * Cerca i consigli da tutti gli utenti per un certo libro.
      *
-     * @param titolo il titolo del libro che si vuole cercare
-     * @return una collection di consigli da parte di tutti gli utenti relativi al libro cercato
-     * @throws IOException
+     * @param titolo il titolo del libro che si vuole cercare.
+     * @return una collection di consigli da parte di tutti gli utenti relativi al libro cercato.
+     * @throws IOException.
      */
     public List<Consiglio> visualizzaConsigliByLibro(String titolo) throws IOException {
         List<Consiglio> userConsigli = new JsonUtils().getConsigli();
@@ -178,11 +178,17 @@ public class User {
         return consigliFound;
     }
 
+    /**
+    * Metodo per la visualizzazione dei consigli di una libreria.
+    *
+    * @param nome della libreria.
+    * @return una lista di consigli.
+    * @throws IOException in caso di errori nella ricerca/return.
+    */
     public List<Consiglio> visualizzaConsigliByLibreria(String nome) throws IOException {
         List<Libreria> userLibs = new JsonUtils().getLibrerie();
         List<Consiglio> consigliLibreria = new ArrayList<>();
         Libreria lib = null;
-        // Trova la libreria
         for(Libreria l : userLibs) {
             if(l.getNome().equals(nome)) {  
                 lib = l;
@@ -198,7 +204,14 @@ public class User {
         }
         return consigliLibreria;
     }
-
+    
+    /**
+    * Metodo per cercare una libreria utente tramite nome della libreria.
+    * 
+    * @param il nome della libreria.
+    * @return la libreria in caso di successo.
+    * @throws IOException in caso di errori durante la ricerca / return dei risultati.
+    */
     public Libreria searchLibByNome(String nomeLib) throws IOException {    
         List<Libreria> userLibs = new JsonUtils().getLibrerie();
         for(Libreria lib : userLibs) {
@@ -208,7 +221,15 @@ public class User {
         }
         return null;    
     }
-
+    
+    /**
+    * Metodo per la ricerca di recensioni tramite nome, cognome del pubblicatore
+    * e titolo del libro.
+    *
+    * @param nome e cognome dell'utente che ha creato la libreria, titolo del libro.
+    * @return la recensione (se esiste).
+    * @throws IOException in caso di errori durante la ricerca / return dei risultati.
+    */
     public Recensione searchRecensioneByUserTitolo(String nome, String cognome, String titoloLibro) throws IOException {   
         List<Recensione> allReviews = new JsonUtils().getRecensioni();  
         for(Recensione review : allReviews) {
