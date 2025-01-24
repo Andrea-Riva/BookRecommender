af615de22a4c3ba51874b68d5fe1d0eade851702
CostaAris <ariscosta2002@gmail.com>
2025-01-24 16:19:16 +0100

    Update DettagliLibreriaController.java

--- src/main/java/org/BookRecommender/Controller/library/DettagliLibreriaController.java
+++ src/main/java/org/BookRecommender/Controller/library/DettagliLibreriaController.java
@@ -14,7 +14,9 @@ import org.BookRecommender.User;
 
 import java.io.IOException;
 import java.util.Objects;
-
+/**
+* Classe controller per i dettagli della libreria di un utente.
+*/
 public class DettagliLibreriaController {
     @FXML
     public GridPane bookGridPane;
@@ -24,17 +26,22 @@ public class DettagliLibreriaController {
     public Label loggedUserLabel;
     @FXML
     public Label nomeLibLabel;
+    /**
+   * Inizializza la visualizzazione dei dettagli della libreria e dei suoi libri.
+   * 
+   * Questo metodo verifica se un utente è loggato, visualizza la sua email,
+   * imposta il nome della libreria selezionata e popola una griglia con i libri contenuti.
+   * Ogni libro è rappresentato da un'etichetta e un pulsante per visualizzarne i dettagli.
+   */
 
     @FXML
     private void initialize() {
-        if(!(Objects.isNull(LoggedUserModel.user))) {   // Utente loggato
-            loggedUserLabel.setText(LoggedUserModel.user.getMail());    // Display mail
+        if(!(Objects.isNull(LoggedUserModel.user))) {   
+            loggedUserLabel.setText(LoggedUserModel.user.getMail());    
         }
-        // Set nome della libreria
         nomeLibLabel.setText(LibreriaModel.libreria.getNome());
-        // Display libri della libreria del bottone clickato
-        int row = 0;    // Counter
-        for(Libro libro : LibreriaModel.libreria.getLibri()) {  // Scorre tutti i libri presenti nella libreria
+        int row = 0;    
+        for(Libro libro : LibreriaModel.libreria.getLibri()) {  
             Label libroLabel = new Label();
             Button libroButton = new Button();
 
@@ -43,11 +50,9 @@ public class DettagliLibreriaController {
 
             bookGridPane.add(libroLabel, 0, row);
             bookGridPane.add(libroButton, 1, row);
-            row++; // Aumenta il contatore
+            row++; 
 
-            // Attribuisce OnAction al button
-            libroButton.setOnAction(actionEvent -> { // L'evento apre una nuova pagina FXML e fa il display dei dettagli del libro
-                // Assegna il campo Libro a LibroModel
+            libroButton.setOnAction(actionEvent -> {
                 try {
                     LibroModel.libro = new User().searchLibroByTitolo(libroLabel.getText());
                     System.out.println(libroButton.getText());
@@ -55,7 +60,6 @@ public class DettagliLibreriaController {
                 } catch (Exception e) {
                     throw new RuntimeException(e);
                 }
-                // Switch scene
                 try {
                     new SceneSwitch(anchorPane, "/org/BookRecommender/View/dettagliLibro/dettagliLibroFromLib.fxml");
                 } catch (IOException e) {
@@ -64,14 +68,26 @@ public class DettagliLibreriaController {
             });
         }
     }
-
+    
+    /**
+    * Metodo per la gestione del pulsante "indietro"
+    * Effettua uno switch della scena visualizzado tutte le librerie.
+    *
+    * @throws exception in caso di anomalie durante l'esecuzione del metodo.
+    */
     @FXML
-    private void goToAllLibs() throws IOException {    // Click btn Indietro
+    private void goToAllLibs() throws IOException {    
             new SceneSwitch(anchorPane, "/org/BookRecommender/View/library/allUsersLibs.fxml");
     }
-
+    
+      /**
+    * Metodo per la gestione del pulsante "MyLibs"
+    * Effettua uno switch della scena visualizzado le librerie dell'utente.
+    *
+    * @throws exception in caso di anomalie durante l'esecuzione del metodo.
+    */
     @FXML
-    private void goToMyLibs() throws IOException {  // Click btn MyLibs from mieLibrerie.fxml
+    private void goToMyLibs() throws IOException {  
         new SceneSwitch(anchorPane, "/org/BookRecommender/View/library/mieLibrerie.fxml");
     }
 }
